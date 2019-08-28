e6a26b1a4525b3f54cf3ee903895a8b02ebb1187
s-haya-123 <ganbaru.wakenai@gmail.com>
2019-08-28 23:46:02 +0900

    set direct select widget

--- lib/main.dart
+++ lib/main.dart
@@ -21,36 +21,103 @@ class MyApp extends StatelessWidget {
 
 class _watHomePage extends StatelessWidget {
   final elements1 = [
-    "Breakfast",
-    "Lunch",
-    "2nd Snack",
-    "Dinner",
-    "3rd Snack",
+    "5ooW",
+    "600W",
+    "700W",
   ];
+  List<Widget> _buildItems1() {
+    return elements1
+        .map((val) => MySelectionItem(
+      title: val,
+    ))
+        .toList();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('title'),
       ),
-      body: Center(
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: Text(
+                    "To which meal?",
+                    style: TextStyle(
+                        color: Colors.grey, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                DirectSelect(
+                    itemExtent: 35.0,
+                    selectedIndex: 0,
+                    child: MySelectionItem(
+                      isForList: false,
+                      title: elements1[0],
+                    ),
+                    onSelectedItemChanged: (index) {
+//                      setState(() {
+//                        selectedIndex1 = index;
+//                      });
+                    },
+                    items: _buildItems1()),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, top: 20.0),
+                  child: Text(
+                    "Search our database by name",
+                    style: TextStyle(
+                        color: Colors.grey, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ]),
         ),
-      ),// This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
 
+class MySelectionItem extends StatelessWidget {
+  final String title;
+  final bool isForList;
 
+  const MySelectionItem({Key key, this.title, this.isForList = true})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60.0,
+      child: isForList
+          ? Padding(
+        child: _buildItem(context),
+        padding: EdgeInsets.all(10.0),
+      )
+          : Card(
+        margin: EdgeInsets.symmetric(horizontal: 10.0),
+        child: Stack(
+          children: <Widget>[
+            _buildItem(context),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Icon(Icons.arrow_drop_down),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _buildItem(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      alignment: Alignment.center,
+      child: Text(title),
+    );
+  }
+}
 
