507a31948aba8516a6f219ff621b4110ed63d710
s-haya-123 <ganbaru.wakenai@gmail.com>
2019-08-28 23:11:31 +0900

    add direct sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:convert_watt/wattModel.dart';
+import 'package:direct_select/direct_select.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,6 +20,13 @@ class MyApp extends StatelessWidget {
 }
 
 class _watHomePage extends StatelessWidget {
+  final elements1 = [
+    "Breakfast",
+    "Lunch",
+    "2nd Snack",
+    "Dinner",
+    "3rd Snack",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,5 +51,6 @@ class _watHomePage extends StatelessWidget {
     );
   }
 }
+
 
 
